34842833b750a34f30dc0c095dc46eac083a36bf
Nikhil Mittu <nick@mittudev.com>
2020-06-17 19:24:44 +0000

    Fix jshint issues.

--- src/main/webapp/workspace/workspace.js
+++ src/main/webapp/workspace/workspace.js
@@ -1,5 +1,25 @@
 let currentEditor;
 
+function createFirepad(ref, parent, language) {
+  const editor = monaco.editor.create(parent, {
+    language: language,
+    theme: "vs-dark"
+  });
+
+  Firepad.fromMonaco(ref, editor);
+  currentEditor = editor;
+}
+
+function getFirebaseRef() {
+  const workspaceID = getParam("workspaceID");
+  if (workspaceID !== null) {
+    return firebase.database().ref().child(workspaceID);
+  } else {
+    // If we were not given a workspace ID redirect to the home page.
+    window.location.href = "/";
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const config = {
     apiKey: 'AIzaSyA1r_PfVDCXfTgoUNisci5Ag2MKEEwsZCE',
@@ -18,24 +38,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.onresize = () => {
   currentEditor.layout();
-}
-
-function createFirepad(ref, parent, language) {
-  const editor = monaco.editor.create(parent, {
-    language: language,
-    theme: "vs-dark"
-  });
-
-  Firepad.fromMonaco(ref, editor);
-  currentEditor = editor;
-}
-
-function getFirebaseRef() {
-  const workspaceID = getParam("workspaceID");
-  if (workspaceID != null) {
-    return firebase.database().ref().child(workspaceID);
-  } else {
-    // If we were not given a workspace ID redirect to the home page.
-    window.location.href = "/"
-  }
-}
+};
