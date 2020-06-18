ab1095191f61d728ad2d45e3d8591cdce8354053
Bianca Saputra <bsaputra132@gmail.com>
2020-06-18 18:15:31 +0000

    Remove wildcard imports

--- src/main/java/com/google/sps/servlets/EnterQueue.java
+++ src/main/java/com/google/sps/servlets/EnterQueue.java
@@ -7,7 +7,7 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
