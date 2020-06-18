5ce0b5ab956e85fc2f0b67d4368869d8456d5e81
Maryam <amaryam294@gmail.com>
2020-06-18 16:53:01 +0000

    Move getCookie() into helper class RequestUtils.

--- src/main/java/com/google/sps/servlets/RequestUtils.java
+++ src/main/java/com/google/sps/servlets/RequestUtils.java
@@ -1,21 +1,10 @@
 package com.google.sps.servlets;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
-import com.google.sps.firebase.FirebaseAppManager;
 import javax.servlet.http.Cookie;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-public class RequestUtils extends HttpServlet{
+public class RequestUtils extends HttpServlet {
   // Obtain the cookie from the current session
   public static String getCookie(HttpServletRequest request, String name, String defaultValue) {
     Cookie[] cookies = request.getCookies();
--- src/main/java/com/google/sps/servlets/SessionInit.java
+++ src/main/java/com/google/sps/servlets/SessionInit.java
@@ -1,36 +1,8 @@
 package com.google.sps.servlets;
 
-import com.google.cloud.language.v1.Document;
-import java.io.IOException;
+import com.google.appengine.api.datastore.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.SessionCookieOptions;
-import java.util.concurrent.TimeUnit;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import com.google.sps.firebase.FirebaseAppManager;
-import java.io.PrintWriter;
-import com.google.appengine.api.datastore.*;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import com.google.gson.Gson;
 
 @WebServlet("/init")
-public class SessionInit extends HttpServlet {
-}
+public class SessionInit extends HttpServlet {}
