de15d7dd456380ef07dd1a478ddd4a2e0742f179
Maryam <amaryam294@gmail.com>
2020-06-19 15:35:31 +0000

    Add getParameter() to SessionInit.

--- src/main/java/com/google/sps/servlets/SessionInit.java
+++ src/main/java/com/google/sps/servlets/SessionInit.java
@@ -1,7 +1,26 @@
 package com.google.sps.servlets;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.SessionCookieOptions;
+import com.google.sps.firebase.FirebaseAppManager;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/init")
-public class SessionInit extends HttpServlet {}
+public class SessionInit extends HttpServlet {
+  // Return empty string if no comment, otherwise return text
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    } else {
+      return value;
+    }
+  }
+}
