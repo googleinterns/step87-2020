56016d4d623ef9b85eee0114d3465b00fded1624
Maryam <amaryam294@gmail.com>
2020-06-19 15:51:46 +0000

    Make current cookie expire for logging out.

--- src/main/java/com/google/sps/servlets/Logout.java
+++ src/main/java/com/google/sps/servlets/Logout.java
@@ -1,26 +1,20 @@
 package com.google.sps.servlets;
 
-import java.net.*; 
-import com.google.cloud.language.v1.Document;
-import java.io.PrintWriter;
-import com.google.appengine.api.datastore.*;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.firebase.auth.FirebaseToken;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.SessionCookieOptions;
-import java.util.concurrent.TimeUnit;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import com.google.sps.firebase.FirebaseAppManager;
 
-
+@WebServlet("/sign-out")
+public class Logout extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    final int maxAge = 0;
+    Cookie newCookie = new Cookie("session", " ");
+    newCookie.setMaxAge(maxAge); // Set the expiration date to immediate deletion
+    response.addCookie(newCookie);
+    response.sendRedirect("/index.html"); // Go back to the main page
+  }
+}
