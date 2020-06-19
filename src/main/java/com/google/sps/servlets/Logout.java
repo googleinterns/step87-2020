655009f17ebdcc146cf330aa511ee02a0dfdc4e7
Maryam <amaryam294@gmail.com>
2020-06-19 19:27:52 +0000

    Set session cookie to an empty string w/o space.

--- src/main/java/com/google/sps/servlets/Logout.java
+++ src/main/java/com/google/sps/servlets/Logout.java
@@ -13,7 +13,7 @@ public class Logout extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     final int maxAge = 0;
-    Cookie newCookie = new Cookie("session", " ");
+    Cookie newCookie = new Cookie("session", "");
     newCookie.setMaxAge(maxAge); // Set the expiration date to immediate deletion
     response.addCookie(newCookie);
     response.sendRedirect("/index.html"); // Go back to the main page
