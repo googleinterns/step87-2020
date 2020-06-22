25944526ecb5921334b901b589494cd5155d6efd
Nikhil Mittu <nick@mittudev.com>
2020-06-22 13:58:45 +0000

    Automatically add new tabs when when realtime database is updated with new files.

--- src/main/webapp/workspace/workspace.js
+++ src/main/webapp/workspace/workspace.js
@@ -19,37 +19,6 @@ function scrollTabs(event) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("tabs-container").onwheel = scrollTabs;
-
-  const config = {
-    apiKey: 'AIzaSyA1r_PfVDCXfTgoUNisci5Ag2MKEEwsZCE',
-    databaseURL: "https://fulfillment-deco-step-2020.firebaseio.com",
-    projectId: "fulfillment-deco-step-2020",
-  };
-  firebase.initializeApp(config);
-
-  const ref = getFirebaseRef();
-
-  require.config({ paths: {'vs': 'https://unpkg.com/monaco-editor@latest/min/vs'}});
-  require(['vs/editor/editor.main'], function() {
-    // Once the monaco library is loaded, we can start uploading files.
-    const hiddenElements = document.getElementsByClassName("initially-hidden");
-
-    for (let e of hiddenElements) {
-      e.classList.remove("initially-hidden");
-    }
-  });
-});
-
-window.onresize = () => {
-  tabs[currentTab].editor.layout();
-};
-
-function uploadFiles() {
-  document.getElementById("upload-files").click();
-}
-
 function switchTab(tab) {
   if (currentTab !== tab) {
     if (tabs[currentTab]) {
@@ -79,14 +48,14 @@ function decodeFileName(filename) {
   return decodeURIComponent(filename.replace("%2E", "."));
 }
 
-function createNewTab(file) {
-  if (!tabs[file.name]) {
+function createNewTab(filename, contents) {
+  if (!tabs[filename]) {
     const tab = document.createElement("button");
     tab.classList.add("inactive-tab", "tab");
-    tab.innerText = file.name;
+    tab.innerText = filename;
 
     tab.onclick = (event) => {
-      switchTab(file.name);
+      switchTab(filename);
     };
 
     const firepadContainer = document.createElement("div");
@@ -97,15 +66,15 @@ function createNewTab(file) {
       theme: "vs-dark"
     });
     
-    const firepad = Firepad.fromMonaco(getFirebaseRef().child(encodeFileName(file.name)), editor);
+    const firepad = Firepad.fromMonaco(getFirebaseRef().child(encodeFileName(filename)), editor);
 
-    firepad.on("ready", () => {
-      file.text().then((contents) => {
+    if (contents !== null) {
+      firepad.on("ready", () => {
         firepad.setText(contents);
       });
-    });
+    }
 
-    tabs[file.name] = {
+    tabs[filename] = {
       editor: editor,
       firepad: firepad,
       tabButton: tab,
@@ -114,19 +83,58 @@ function createNewTab(file) {
 
     document.getElementById("tabs-container").appendChild(tab);
     document.getElementById("firepads").appendChild(firepadContainer);
-  } else {
-    file.text().then(contents => {
-      tabs[file.name].firepad.setText(contents);
-    });
+  } else if (contents !== null) {
+    tabs[filename].firepad.setText(contents);
   }
 
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("tabs-container").onwheel = scrollTabs;
+
+  const config = {
+    apiKey: 'AIzaSyA1r_PfVDCXfTgoUNisci5Ag2MKEEwsZCE',
+    databaseURL: "https://fulfillment-deco-step-2020.firebaseio.com",
+    projectId: "fulfillment-deco-step-2020",
+  };
+  firebase.initializeApp(config);
+
+  require.config({ paths: {'vs': 'https://unpkg.com/monaco-editor@latest/min/vs'}});
+  require(['vs/editor/editor.main'], function() {
+    // Once the monaco library is loaded, we can start uploading files.
+    const hiddenElements = document.getElementsByClassName("initially-hidden");
+
+    for (let e of hiddenElements) {
+      e.classList.remove("initially-hidden");
+    }
+
+    getFirebaseRef().on("child_added", (snapshot) => {
+      const filename = decodeFileName(snapshot.key);
+      createNewTab(filename, null);
+      if (!currentTab) {
+        switchTab(filename);
+      }
+    });
+  });
+});
+
+window.onresize = () => {
+  if (currentTab) {
+    tabs[currentTab].editor.layout();
+  }
+};
+
+function uploadFiles() {
+  document.getElementById("upload-files").click();
 }
 
 function filesUploaded() {
   const files = document.getElementById("upload-files").files;
 
   for(var file of files) {
-    createNewTab(file);
+    file.text().then(contents => {
+      createNewTab(file.name, contents);
+    });
   }
 
   switchTab(files[0].name);
