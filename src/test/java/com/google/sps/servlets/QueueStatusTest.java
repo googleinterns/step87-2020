056dcf30b5a7a883970c53861348605967b906fa
Bianca Saputra <bsaputra132@gmail.com>
2020-06-24 14:43:04 +0000

    Add test cases for CheckStudentStatus.java

---
+++ src/test/java/com/google/sps/servlets/QueueStatusTest.java
@@ -0,0 +1,141 @@
+package com.google.sps.servlets;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class QueueStatusTest {
+
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  private DatastoreService datastore;
+
+  @Mock HttpServletRequest httpRequest;
+
+  @Mock HttpServletResponse httpResponse;
+
+  @Mock FirebaseAuth authInstance;
+
+  @InjectMocks CheckStudentStatus queue;
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  @Test
+  public void firstInQueue() throws Exception {
+    Entity init = new Entity("Class");
+    ArrayList<String> setQueue = new ArrayList<String>(Arrays.asList("uID"));
+
+    init.setProperty("owner", "ownerID");
+    init.setProperty("name", "testClass");
+    init.setProperty("beingHelped", "");
+    init.setProperty("studentQueue", setQueue);
+
+    datastore.put(init);
+
+    when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
+    when(httpRequest.getParameter("userToken")).thenReturn("testID");
+
+    FirebaseToken mockToken = mock(FirebaseToken.class);
+    when(authInstance.verifyIdToken("testID")).thenReturn(mockToken);
+    when(mockToken.getUid()).thenReturn("uID");
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(httpResponse.getWriter()).thenReturn(writer);
+
+    queue.doGet(httpRequest, httpResponse);
+
+    assertEquals(stringWriter.toString(), "1");
+  }
+
+  @Test
+  public void midQueue() throws Exception {
+    Entity init = new Entity("Class");
+    ArrayList<String> setQueue =
+        new ArrayList<String>(Arrays.asList("test1", "test2", "uID", "test3"));
+
+    init.setProperty("owner", "ownerID");
+    init.setProperty("name", "testClass");
+    init.setProperty("beingHelped", "");
+    init.setProperty("studentQueue", setQueue);
+
+    datastore.put(init);
+
+    when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
+    when(httpRequest.getParameter("userToken")).thenReturn("testID");
+
+    FirebaseToken mockToken = mock(FirebaseToken.class);
+    when(authInstance.verifyIdToken("testID")).thenReturn(mockToken);
+    when(mockToken.getUid()).thenReturn("uID");
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(httpResponse.getWriter()).thenReturn(writer);
+
+    queue.doGet(httpRequest, httpResponse);
+
+    assertEquals(stringWriter.toString(), "3");
+  }
+
+  @Test
+  public void duplicateInQueue() throws Exception {
+    Entity init = new Entity("Class");
+    ArrayList<String> setQueue =
+        new ArrayList<String>(Arrays.asList("uID", "test2", "uID", "test3"));
+
+    init.setProperty("owner", "ownerID");
+    init.setProperty("name", "testClass");
+    init.setProperty("beingHelped", "");
+    init.setProperty("studentQueue", setQueue);
+
+    datastore.put(init);
+
+    when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
+    when(httpRequest.getParameter("userToken")).thenReturn("testID");
+
+    FirebaseToken mockToken = mock(FirebaseToken.class);
+    when(authInstance.verifyIdToken("testID")).thenReturn(mockToken);
+    when(mockToken.getUid()).thenReturn("uID");
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(httpResponse.getWriter()).thenReturn(writer);
+
+    queue.doGet(httpRequest, httpResponse);
+
+    assertEquals(stringWriter.toString(), "1");
+  }
+}
