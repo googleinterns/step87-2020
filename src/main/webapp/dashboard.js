b57ea1c8fc5958eaf1ae045a76afe8cd9f2c0bab
Maryam <amaryam294@gmail.com>
2020-06-24 15:11:26 +0000

    Center the visits chart on admin dashboard.

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -1,8 +1,8 @@
 google.charts.load('current', {'packages':['corechart']});
 
 function drawChart() {
-  var visits = [5, 10, 15,20,10];
-  var days = ['1', '2', '3','4','5'];
+  // Instead of the following, get the visits and days array from localStorage
+  var visits = [5, 10, 7,20,10];
 
   var data=[];
 
@@ -10,7 +10,7 @@ function drawChart() {
 
   data.push(Header);
 
-  for (var i = 0; i < days.length; i++) {
+  for (var i = 0; i < visits.length; i++) {
     var temp=[];
     temp.push(i.toString());
     temp.push(visits[i]);
@@ -30,18 +30,3 @@ function drawChart() {
 }
 
 google.charts.setOnLoadCallback(drawChart);
-
-// This function would calculate number of visits to the site
-function incrementVisits() {
-  if (typeof(Storage) !== "undefined") { // Check if browser supports local storage
-    // if (localStorage.clickcount) {
-    //   localStorage.clickcount = Number(localStorage.clickcount)+1;
-    // } else {
-    //   localStorage.clickcount = 1;
-    // }
-   // console.log('There are' + localStorage.clickcount + 'visits to the site');
-   console.log('Browser suppports web storage!');
-  } else {
-    console.log('Browser does not support web storage'); 
-  }
-}
