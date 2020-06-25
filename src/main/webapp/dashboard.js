3fdd8e937840660ab52be3e73e655c9af2d31009
Maryam <amaryam294@gmail.com>
2020-06-25 18:16:39 +0000

    Change chart background to gradient.

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -24,6 +24,22 @@ function drawChart() {
     legend: { position: 'right' },
     hAxis: {title: 'Day' },
     vAxis: {title: 'Number of Visits' },
+    backgroundColor: {
+      gradient: {
+        // Start color for gradient
+        color1: '#fcf7b6',
+        // Finish color for gradient
+        color2: '#4ccd88',
+        // Start and end point of gradient, start 
+        // on upper left corner
+        x1: '0%', y1: '0%',
+        x2: '100%', y2: '100%',
+        // If true, the boundary for x1,
+        // y1, x2, and y2 is the box. If
+        // false, it's the entire chart.
+        useObjectBoundingBoxUnits: true
+      },
+    },
   };
 
   var chart = new google.visualization.LineChart(document.getElementById('line-chart'));
