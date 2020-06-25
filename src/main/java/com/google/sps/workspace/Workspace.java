ca3d5cba4232cdb83d936d010db918844c69cf27
Nikhil Mittu <nick@mittudev.com>
2020-06-25 19:59:13 +0000

    Add method to get an archive of the workspace.

--- src/main/java/com/google/sps/workspace/Workspace.java
+++ src/main/java/com/google/sps/workspace/Workspace.java
@@ -7,12 +7,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.sps.firebase.FirebaseAppManager;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.zip.GZIPOutputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 
 /**
  * Provides an interface to access and modify workspaces in the datastore. The contents of this
@@ -84,8 +89,8 @@ public class Workspace {
     return future;
   }
 
-  public Future<List<String>> getFiles() {
-    CompletableFuture<List<String>> future = new CompletableFuture<>();
+  public Future<List<WorkspaceFile>> getFiles() {
+    CompletableFuture<List<WorkspaceFile>> future = new CompletableFuture<>();
 
     reference
         .child("files")
@@ -94,12 +99,16 @@ public class Workspace {
 
               @Override
               public void onDataChange(DataSnapshot snapshot) {
-                ArrayList<String> children = new ArrayList<>();
-                for (DataSnapshot child : snapshot.getChildren()) {
-                  children.add(child.getKey());
-                }
+                try {
+                  ArrayList<WorkspaceFile> children = new ArrayList<>();
+                  for (DataSnapshot child : snapshot.getChildren()) {
+                    children.add(new WorkspaceFile(child));
+                  }
 
-                future.complete(children);
+                  future.complete(children);
+                } catch (Exception e) {
+                  future.completeExceptionally(e);
+                }
               }
 
               @Override
@@ -109,6 +118,30 @@ public class Workspace {
             });
 
     return future;
+  }
+
+  public String getArchive(OutputStream out)
+      throws InterruptedException, ExecutionException, IOException {
+    List<WorkspaceFile> files = getFiles().get();
+
+    TarArchiveOutputStream tarOut =
+        new TarArchiveOutputStream(new GZIPOutputStream(new BufferedOutputStream(out)));
+
+    for (WorkspaceFile file : files) {
+      byte contents[] = file.getContents().get().getBytes();
+
+      TarArchiveEntry entry = new TarArchiveEntry(file.getFilename());
+      entry.setSize(contents.length);
+
+      tarOut.putArchiveEntry(entry);
+      tarOut.write(contents);
+
+      tarOut.closeArchiveEntry();
+    }
+
+    tarOut.close();
+
+    return "";
   }
 
   /** @return the workspaceID */
