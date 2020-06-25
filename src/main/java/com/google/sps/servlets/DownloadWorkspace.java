2355a7859c56d543bfe635528841a49778c0627c
Nikhil Mittu <nick@mittudev.com>
2020-06-25 19:58:27 +0000

    Add servlet to download a workspace.

---
+++ src/main/java/com/google/sps/servlets/DownloadWorkspace.java
@@ -0,0 +1,26 @@
+package com.google.sps.servlets;
+
+import com.google.sps.workspace.Workspace;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/workspace/downloadWorkspace")
+public class DownloadWorkspace extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    Workspace w = new Workspace(req.getParameter("workspaceID"));
+
+    resp.setContentType("application/x-gzip");
+
+    try {
+      w.getArchive(resp.getOutputStream());
+    } catch (InterruptedException | ExecutionException e) {
+      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
