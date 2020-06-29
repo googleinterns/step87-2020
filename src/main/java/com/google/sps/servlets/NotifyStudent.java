2d09f2502bafdf2672beeb458adf82e31cc7d0a8
Bianca Saputra <biancasap@google.com>
2020-06-29 18:41:50 +0000

    Add servlet to take student off of queue

--- src/main/java/com/google/sps/servlets/NotifyStudent.java
+++ src/main/java/com/google/sps/servlets/NotifyStudent.java
@@ -3,9 +3,18 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceConfig;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Transaction;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import com.google.sps.firebase.FirebaseAppManager;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,5 +45,55 @@ public class NotifyStudent extends HttpServlet {
     datastore = DatastoreServiceFactory.getDatastoreService();
     System.setProperty(
         DatastoreServiceConfig.DATASTORE_EMPTY_LIST_SUPPORT, Boolean.TRUE.toString());
+
+    try {
+      String classCode = request.getParameter("classCode").trim();
+
+      int retries = 10;
+      while (true) {
+        Transaction txn = datastore.beginTransaction();
+        try {
+          // Retrive class entity
+          Key classKey = KeyFactory.stringToKey(classCode);
+          Entity classEntity = datastore.get(txn, classKey);
+
+          // Get uID from uEmail
+          String uEmail = request.getParameter("uEmail");
+          UserRecord userRecord = authInstance.getUserByEmail(uEmail);
+          String uID = userRecord.getUid();
+
+          // Update queue
+          ArrayList<String> updatedQueue = (ArrayList) classEntity.getProperty("studentQueue");
+          updatedQueue.remove(uID);
+
+          // Update beingHelped
+          ArrayList<String> beingHelped = (ArrayList) classEntity.getProperty("beingHelped");
+          beingHelped.add(uID);
+
+          classEntity.setProperty("studentQueue", updatedQueue);
+          classEntity.setProperty("beingHelped", beingHelped);
+          datastore.put(txn, classEntity);
+
+          txn.commit();
+          break;
+        } catch (ConcurrentModificationException e) {
+          if (retries == 0) {
+            throw e;
+          }
+          // Allow retry to occur
+          --retries;
+        } finally {
+          if (txn.isActive()) {
+            txn.rollback();
+          }
+        }
+      }
+    } catch (EntityNotFoundException e) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    } catch (IllegalArgumentException e) {
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+    } catch (FirebaseAuthException e) {
+      response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
   }
 }
