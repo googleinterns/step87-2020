7ad77a8f736c255e5d4eb09f6c09dcf7ebb622d0
Nikhil Mittu <nick@mittudev.com>
2020-06-29 19:47:01 +0000

    Refacor code for testing.

--- src/main/java/com/google/sps/servlets/DownloadWorkspace.java
+++ src/main/java/com/google/sps/servlets/DownloadWorkspace.java
@@ -3,7 +3,6 @@ package com.google.sps.servlets;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
-import com.google.sps.workspace.WorkspaceFactory;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,22 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/workspace/downloadWorkspace")
 public class DownloadWorkspace extends HttpServlet {
-  WorkspaceFactory workspaceFactory;
+  private BlobstoreService blobstoreService;
 
   @Override
   public void init() throws ServletException {
     // TODO Auto-generated method stub
     super.init();
-    workspaceFactory = WorkspaceFactory.getInstance();
+    blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
   }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    String downloadID = req.getParameter("downloadID");
+    String downloadID = req.getParameter("filename");
 
     resp.setContentType("application/x-gzip");
 
-    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     BlobKey blobKey =
         blobstoreService.createGsBlobKey(
             "/gs/fulfillment-deco-step-2020.appspot.com/" + downloadID);
