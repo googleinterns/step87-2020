60e5929541f7a740b182dbdd6471f16702f390ed
Maryam <amaryam294@gmail.com>
2020-06-29 22:03:42 +0000

    Test JSON Output for one class.

--- src/test/java/com/google/sps/servlets/VisitTest.java
+++ src/test/java/com/google/sps/servlets/VisitTest.java
@@ -1,7 +1,8 @@
-package com.google.sps.servlets;
+package com.google.sps;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -10,6 +11,8 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +20,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -31,6 +35,8 @@ public class VisitTest {
   @Mock HttpServletRequest httpRequest;
 
   @Mock HttpServletResponse httpResponse;
+
+  @InjectMocks Visits checkVisits;
 
   @Before
   public void setUp() {
@@ -72,6 +78,45 @@ public class VisitTest {
 
     assertEquals((String) "exampleClassName", (String) listOfClassNames.get(0));
     assertEquals((long) 15, (long) visitsPerClass.get(0));
+  }
+
+  @Test
+  // 1 Class w/ JSON Output
+  public void checkVisitJSONOutput() throws Exception {
+    ArrayList<String> listOfClassNames = new ArrayList<String>();
+    ArrayList<Long> visitsPerClass = new ArrayList<Long>();
+
+    Entity visitEntity = new Entity("Visit");
+    visitEntity.setProperty("classKey", "testClass101");
+    visitEntity.setProperty("numVisits", 20);
+    visitEntity.setProperty("className", "exampleClassName");
+
+    datastore.put(visitEntity);
+
+    // Obtain visits from datastore and filter them into results query
+    Query query = new Query("Visit");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // Store the class name and number of visits into two separate lists
+    for (Entity entity : results.asIterable()) {
+      String className = (String) entity.getProperty("className");
+      long classVisits = (long) entity.getProperty("numVisits");
+
+      listOfClassNames.add(className);
+      visitsPerClass.add(classVisits);
+    }
+
+    assertEquals((String) "exampleClassName", (String) listOfClassNames.get(0));
+    assertEquals((long) 20, (long) visitsPerClass.get(0));
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(httpResponse.getWriter()).thenReturn(writer);
+
+    checkVisits.doGet(httpRequest, httpResponse);
+    assertTrue(stringWriter.toString().contains("exampleClassName"));
+    assertTrue(stringWriter.toString().contains("20"));
   }
 
   @Test
