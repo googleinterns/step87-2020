87e7722bedb66961eab2b8db40a5825315f2ac48
Nikhil Mittu <nick@mittudev.com>
2020-07-01 19:56:43 +0000

    Add more unit tests for WorkspaceArchive.

--- src/test/java/com/google/sps/workspace/WorkspaceArchiveTest.java
+++ src/test/java/com/google/sps/workspace/WorkspaceArchiveTest.java
@@ -2,17 +2,24 @@ package com.google.sps.workspace;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.sps.workspace.WorkspaceArchive.ArchiveType;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -23,7 +30,8 @@ public class WorkspaceArchiveTest {
   @Mock Workspace workspace;
   @Mock Future<List<WorkspaceFile>> futureFiles;
   @Mock Future<String> futureString;
-  @Mock TarArchiveOutputStream tarOut;
+  @Mock ArchiveOutputStream archiveOut;
+  @Mock OutputStream out;
   @Mock WorkspaceFile file;
 
   @Test(expected = NullPointerException.class)
@@ -37,7 +45,7 @@ public class WorkspaceArchiveTest {
   }
 
   @Test
-  public void archive() throws InterruptedException, ExecutionException, IOException {
+  public void archiveTar() throws InterruptedException, ExecutionException, IOException {
     final int NUM_FILES = 3;
     final String FILENAME = "FILENAME";
     final String CONTENTS = "CONTENTS";
@@ -53,17 +61,47 @@ public class WorkspaceArchiveTest {
     when(file.getContents()).thenReturn(futureString);
     when(futureString.get()).thenReturn(CONTENTS);
 
-    new WorkspaceArchive(workspace, ArchiveType.TAR).archive(tarOut);
+    new WorkspaceArchive(workspace, ArchiveType.TAR).archive(archiveOut);
 
     verify(workspace, times(1)).getFiles();
     verify(file, times(NUM_FILES)).getFilename();
     verify(file, times(NUM_FILES)).getContents();
 
-    verify(tarOut, times(NUM_FILES)).putArchiveEntry(any());
-    verify(tarOut, times(NUM_FILES)).write(eq(CONTENTS.getBytes()));
-    verify(tarOut, times(NUM_FILES)).closeArchiveEntry();
+    verify(archiveOut, times(NUM_FILES)).putArchiveEntry(any(TarArchiveEntry.class));
+    verify(archiveOut, times(NUM_FILES)).write(eq(CONTENTS.getBytes()));
+    verify(archiveOut, times(NUM_FILES)).closeArchiveEntry();
 
-    verify(tarOut, times(1)).close();
+    verify(archiveOut, times(1)).close();
+  }
+
+  @Test
+  public void archiveZip() throws InterruptedException, ExecutionException, IOException {
+    final int NUM_FILES = 3;
+    final String FILENAME = "FILENAME";
+    final String CONTENTS = "CONTENTS";
+
+    List<WorkspaceFile> files = new ArrayList<>();
+    for (int i = 0; i < NUM_FILES; i++) {
+      files.add(file);
+    }
+
+    when(workspace.getFiles()).thenReturn(futureFiles);
+    when(futureFiles.get()).thenReturn(files);
+    when(file.getFilename()).thenReturn(FILENAME);
+    when(file.getContents()).thenReturn(futureString);
+    when(futureString.get()).thenReturn(CONTENTS);
+
+    new WorkspaceArchive(workspace, ArchiveType.ZIP).archive(archiveOut);
+
+    verify(workspace, times(1)).getFiles();
+    verify(file, times(NUM_FILES)).getFilename();
+    verify(file, times(NUM_FILES)).getContents();
+
+    verify(archiveOut, times(NUM_FILES)).putArchiveEntry(any(ZipArchiveEntry.class));
+    verify(archiveOut, times(NUM_FILES)).write(eq(CONTENTS.getBytes()));
+    verify(archiveOut, times(NUM_FILES)).closeArchiveEntry();
+
+    verify(archiveOut, times(1)).close();
   }
 
   @Test(expected = ExecutionException.class)
@@ -71,6 +109,26 @@ public class WorkspaceArchiveTest {
     when(workspace.getFiles()).thenReturn(futureFiles);
     when(futureFiles.get()).thenThrow(new ExecutionException(new Exception()));
 
-    new WorkspaceArchive(workspace, ArchiveType.TAR).archive(tarOut);
+    new WorkspaceArchive(workspace, ArchiveType.TAR).archive(archiveOut);
+  }
+
+  @Test
+  public void archiveHelperTar() throws Exception {
+    WorkspaceArchive archive = spy(new WorkspaceArchive(workspace, ArchiveType.TAR));
+    doNothing().when(archive).archive(any(ArchiveOutputStream.class));
+
+    archive.archive(out);
+
+    verify(archive, times(1)).archive(any(TarArchiveOutputStream.class));
+  }
+
+  @Test
+  public void archiveHelperZip() throws Exception {
+    WorkspaceArchive archive = spy(new WorkspaceArchive(workspace, ArchiveType.ZIP));
+    doNothing().when(archive).archive(any(ArchiveOutputStream.class));
+
+    archive.archive(out);
+
+    verify(archive, times(1)).archive(any(ZipArchiveOutputStream.class));
   }
 }
