d9db864b6ede27a7aebe2622947cdde06ea14862
Maryam <amaryam294@gmail.com>
2020-07-10 14:45:50 +0000

    Change background color of chart.

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -29,22 +29,12 @@ function drawBasic2() {
       title: 'Number of Visits per Day',
       hAxis: {title: 'Date'},
       vAxis: {title: 'Number of Visits'},
+      textStyle: {
+        color: 'white'
+      },
       backgroundColor: {
-        gradient: {
-          // Start color for gradient
-          color1: '#fcf7b6',
-          // Finish color for gradient
-          color2: '#4ccd88',
-          // Start and end point of gradient, start 
-          // on upper left corner
-          x1: '0%', y1: '0%',
-          x2: '100%', y2: '100%',
-          // If true, the boundary for x1,
-          // y1, x2, and y2 is the box. If
-          // false, it's the entire chart.
-          useObjectBoundingBoxUnits: true
-        },
-        stroke: '#082f44',
+        color: '#2457AA',
+        stroke: '#031430',
         strokeWidth: 5
       },
     };
