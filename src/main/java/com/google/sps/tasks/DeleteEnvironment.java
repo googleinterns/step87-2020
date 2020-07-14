bd7152f7398e886a1a4da4762084ac0bbdddd91d
Nikhil Mittu <nick@mittudev.com>
2020-07-14 14:55:00 +0000

    Rename DeleteEnvironments to DeleteEnvironment.

--- src/main/java/com/google/sps/tasks/DeleteEnvironment.java
+++ src/main/java/com/google/sps/tasks/DeleteEnvironment.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/tasks/deleteEnv")
-public class DeleteEnvironments extends HttpServlet {
+public class DeleteEnvironment extends HttpServlet {
   DockerClient docker;
 
   @Override
