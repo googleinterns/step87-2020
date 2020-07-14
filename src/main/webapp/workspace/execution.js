3901f657e57716328d1cb5326eff1450bb5df649
Nikhil Mittu <nick@mittudev.com>
2020-07-14 22:18:56 +0000

    Add option to select element.

--- src/main/webapp/workspace/execution.js
+++ src/main/webapp/workspace/execution.js
@@ -16,12 +16,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 getFirebaseRef().child("classID").once("value", snap => {
-  fetch(`/getEnvironments?classID=${snap.val}`).then(resp => resp.json()).then(envs => {
+  fetch(`/getEnvironments?classID=${snap.val()}`).then(resp => resp.json()).then(envs => {
     const select = document.getElementById("envSelect");
     for (var env of envs) {
       const option = document.createElement("option");
       option.value = env.id;
       option.innerText = env.name;
+      select.appendChild(option);
     }
 
     if (envs.length > 0) {
