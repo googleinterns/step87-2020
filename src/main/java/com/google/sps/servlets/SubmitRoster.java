20423a5d69a442c1ce1e4aaa5ba675b2b4d8e31b
Maryam <amaryam294@gmail.com>
2020-07-15 16:54:34 +0000

    Add comments to roster servlet.

--- src/main/java/com/google/sps/servlets/SubmitRoster.java
+++ src/main/java/com/google/sps/servlets/SubmitRoster.java
@@ -5,13 +5,13 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.sps.firebase.FirebaseAppManager;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// Once class owner submits a TA email, retrieve that user and add them as a TA to the class
+// Once class owner submits a roster, add or update those student users class registration
 @WebServlet("/submit-roster")
 public class SubmitRoster extends HttpServlet {
 
@@ -38,7 +38,7 @@ public class SubmitRoster extends HttpServlet {
     }
   }
 
-  // Add a user to the datastore
+  // Add a student user to the datastore
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -52,26 +52,37 @@ public class SubmitRoster extends HttpServlet {
     // Find the corresponding class Key
     String classCode = request.getParameter("classCode").trim();
     Key classKey = KeyFactory.stringToKey(classCode);
-    ArrayList<Key> registered = new ArrayList<Key>();
-    registered.add(classKey);
 
     for (String email : allClassEmails) {
-      // Prevent creating duplicate users
-      Query query =
-          new Query("User")
-              .setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, email));
+      // Look for the TA in the user datastore
+      PreparedQuery queryUser =
+          datastore.prepare(
+              new Query("User")
+                  .setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, email)));
 
-      if (datastore.prepare(query).countEntities() == 0) {
+      Entity user;
 
-        Entity user = new Entity("User");
+      // If the student user entity doesnt exist yet, create one
+      if (queryUser.countEntities() == 0) {
+
+        List<Key> regClassesList = Arrays.asList(classKey);
+
+        user = new Entity("User");
         user.setProperty("userEmail", email);
-        user.setProperty("registeredClasses", registered);
+        user.setProperty("registeredClasses", regClassesList);
         user.setProperty("ownedClasses", Collections.emptyList());
         user.setProperty("taClasses", Collections.emptyList());
 
         datastore.put(user);
-      } else {
-        // Entity user = query.asSingleEntity();
+      }
+      // If student already exists, update their registered class list
+      else {
+        user = queryUser.asSingleEntity();
+        List<Key> regClassesList = (List<Key>) user.getProperty("registeredClasses");
+        regClassesList.add(classKey);
+        user.setProperty("registeredClasses", regClassesList);
+
+        datastore.put(user);
       }
     }
 
