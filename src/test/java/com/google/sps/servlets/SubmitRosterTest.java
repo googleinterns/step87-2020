edbffa5b7515af890a8fe72ecb7440ed6a10def8
Maryam <amaryam294@gmail.com>
2020-07-15 20:25:00 +0000

    Test for prevention of duplicate roster creation.

--- src/test/java/com/google/sps/servlets/SubmitRosterTest.java
+++ src/test/java/com/google/sps/servlets/SubmitRosterTest.java
@@ -1,5 +1,6 @@
 package com.google.sps.servlets;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -14,6 +15,7 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.firebase.auth.FirebaseAuth;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -85,6 +87,67 @@ public class SubmitRosterTest {
         List<Key> testRegistered = (List<Key>) user.getProperty("registeredClasses");
         assertTrue(testRegistered.contains(init.getKey()));
         assertTrue(testRegistered.size() == 1);
+      }
+    }
+  }
+
+  @Test
+  // For existing users, just update registration list
+  public void preventDuplicateStudentRoster() throws Exception {
+
+    // Create a class
+    Entity init = new Entity("Class");
+
+    init.setProperty("owner", "ownerID");
+    init.setProperty("name", "testClass");
+    init.setProperty("beingHelped", new EmbeddedEntity());
+    init.setProperty("studentQueue", Collections.emptyList());
+    init.setProperty("taList", Collections.emptyList());
+
+    datastore.put(init);
+
+    // Example student 1
+    Entity user1 = new Entity("User");
+
+    List<Key> reg1 = Arrays.asList(init.getKey());
+
+    user1.setProperty("userEmail", "test1@google.com");
+    user1.setProperty("registeredClasses", reg1);
+    user1.setProperty("taClasses", Collections.emptyList());
+    user1.setProperty("ownedClasses", Collections.emptyList());
+
+    // Example student 2
+    Entity user2 = new Entity("User");
+
+    List<Key> reg2 = Arrays.asList(init.getKey());
+
+    user2.setProperty("userEmail", "test2@google.com");
+    user2.setProperty("registeredClasses", reg2);
+    user2.setProperty("taClasses", Collections.emptyList());
+    user2.setProperty("ownedClasses", Collections.emptyList());
+
+    datastore.put(user1);
+    datastore.put(user2);
+
+    // Submit a roster of 2 students
+    when(httpRequest.getParameter("roster")).thenReturn("test1@google.com, test2@google.com");
+    when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
+
+    submitRoster.doPost(httpRequest, httpResponse);
+
+    Query query = new Query("User");
+    PreparedQuery results = datastore.prepare(query);
+
+    // Verify size and contents of registeredClass list for each student
+    for (Entity user : results.asIterable()) {
+      if (user.getProperty("userEmail") == "test1@google.com") {
+        List<Key> testRegistered = (List<Key>) user.getProperty("registeredClasses");
+        assertTrue(testRegistered.contains(init.getKey()));
+        assertEquals(1, testRegistered.size());
+      } else if (user.getProperty("userEmail") == "test2@google.com") {
+        List<Key> testRegistered = (List<Key>) user.getProperty("registeredClasses");
+        assertTrue(testRegistered.contains(init.getKey()));
+        assertEquals(1, testRegistered.size());
       }
     }
   }
