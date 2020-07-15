9bafd6ad0e3c8711f10aea6907ebc835d6bfe755
Maryam <amaryam294@gmail.com>
2020-07-15 20:03:14 +0000

    Test multiple classes for ta addition.

--- src/test/java/com/google/sps/servlets/AddClassTATest.java
+++ src/test/java/com/google/sps/servlets/AddClassTATest.java
@@ -82,6 +82,7 @@ public class AddClassTATest {
       if (entity.getProperty("userEmail") == "test@google.com") {
         List<Key> taClasses = (List<Key>) entity.getProperty("taClasses");
         assertTrue(taClasses.contains(KeyFactory.stringToKey("exampleClassCode")));
+        assertTrue(taClasses.size() == 1);
       }
     }
   }
@@ -127,6 +128,86 @@ public class AddClassTATest {
       if (entity.getProperty("userEmail") == "test@google.com") {
         List<Key> taClasses = (List<Key>) entity.getProperty("taClasses");
         assertTrue(taClasses.contains(KeyFactory.stringToKey("exampleClassCode")));
+        assertTrue(taClasses.contains(init.getKey()));
+        assertTrue(taClasses.size() == 2);
+      }
+    }
+  }
+
+  @Test
+  // For a user that already TAs for one class, add more classes
+  public void addMultipleClassKeys() throws Exception {
+
+    // Create multiple classes
+    Entity init = new Entity("Class");
+
+    init.setProperty("owner", "ownerID");
+    init.setProperty("name", "testClass");
+    init.setProperty("beingHelped", new EmbeddedEntity());
+    init.setProperty("studentQueue", Collections.emptyList());
+    init.setProperty("taList", Collections.emptyList());
+
+    Entity init2 = new Entity("Class");
+
+    init2.setProperty("owner", "ownerID2");
+    init2.setProperty("name", "testClass2");
+    init2.setProperty("beingHelped", new EmbeddedEntity());
+    init2.setProperty("studentQueue", Collections.emptyList());
+    init2.setProperty("taList", Collections.emptyList());
+
+    Entity init3 = new Entity("Class");
+
+    init3.setProperty("owner", "ownerID3");
+    init3.setProperty("name", "testClass3");
+    init3.setProperty("beingHelped", new EmbeddedEntity());
+    init3.setProperty("studentQueue", Collections.emptyList());
+    init3.setProperty("taList", Collections.emptyList());
+
+    Entity init4 = new Entity("Class");
+
+    init4.setProperty("owner", "ownerID4");
+    init4.setProperty("name", "testClass4");
+    init4.setProperty("beingHelped", new EmbeddedEntity());
+    init4.setProperty("studentQueue", Collections.emptyList());
+    init4.setProperty("taList", Collections.emptyList());
+
+    datastore.put(init);
+    datastore.put(init2);
+    datastore.put(init3);
+    datastore.put(init4);
+
+    // Create a user
+    Entity user = new Entity("User");
+
+    List<Key> taClassList =
+        Arrays.asList(init.getKey(), init2.getKey(), init3.getKey(), init4.getKey());
+
+    user.setProperty("userEmail", "test@google.com");
+    user.setProperty("registeredClasses", Collections.emptyList());
+    user.setProperty("taClasses", taClassList);
+    user.setProperty("ownedClasses", Collections.emptyList());
+
+    datastore.put(user);
+
+    // Create examples for the TA email and class code
+    when(httpRequest.getParameter("taEmail")).thenReturn("test@google.com");
+    when(httpRequest.getParameter("classCode")).thenReturn("exampleClassCode");
+
+    addTA.doPost(httpRequest, httpResponse);
+
+    Query query = new Query("User");
+    PreparedQuery results = datastore.prepare(query);
+
+    // Add the class key to the user's TA classes list
+    for (Entity entity : results.asIterable()) {
+      if (entity.getProperty("userEmail") == "test@google.com") {
+        List<Key> taClasses = (List<Key>) entity.getProperty("taClasses");
+        assertTrue(taClasses.contains(KeyFactory.stringToKey("exampleClassCode")));
+        assertTrue(taClasses.contains(init.getKey()));
+        assertTrue(taClasses.contains(init2.getKey()));
+        assertTrue(taClasses.contains(init3.getKey()));
+        assertTrue(taClasses.contains(init4.getKey()));
+        assertTrue(taClasses.size() == 5);
       }
     }
   }
