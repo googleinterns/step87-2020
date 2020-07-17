f28307d07f4a6ec02d739b97f0046eeacbd47cd3
Nikhil Mittu <nick@mittudev.com>
2020-07-17 21:14:03 +0000

    Change location to position.

--- src/main/java/com/google/sps/queue/StudentStatus.java
+++ src/main/java/com/google/sps/queue/StudentStatus.java
@@ -1,12 +1,12 @@
 package com.google.sps.queue;
 
 public class StudentStatus {
-  private int location;
+  private int position;
   private String workspace;
   private static final String WORKSPACE = "/workspace/?workspaceID=";
 
-  public StudentStatus(int location, String workspaceID) {
-    this.location = location;
+  public StudentStatus(int position, String workspaceID) {
+    this.position = position;
     this.workspace = WORKSPACE + workspaceID;
   }
 }
