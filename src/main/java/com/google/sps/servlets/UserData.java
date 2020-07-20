038b9b3a3b3b950095213164b0149babec81d3dd
Bianca Saputra <biancasap@google.com>
2020-07-20 20:09:30 +0000

    Add type property

--- src/main/java/com/google/sps/servlets/UserData.java
+++ src/main/java/com/google/sps/servlets/UserData.java
@@ -3,9 +3,11 @@ package com.google.sps.servlets;
 public class UserData {
   private String name;
   private String code;
+  private String type;
 
-  public UserData(String code, String name) {
+  public UserData(String code, String name, String type) {
     this.name = name;
     this.code = code;
+    this.type = type;
   }
 }
