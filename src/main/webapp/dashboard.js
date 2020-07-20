7a4fa578397fd03ba058fc316fd1e3bce94fc68a
Maryam <amaryam294@gmail.com>
2020-07-20 15:56:35 +0000

    Add chart rendering js for wait time chart.

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -75,7 +75,82 @@ function drawBasic() {
   });
 }
 
+// Render a chart visual on dashboard page for tracking average wait time by date
+function drawTime() {
+
+  // Set up the data table to have a class name and wait average associated w/ that specific class
+  var data = new google.visualization.DataTable();
+  data.addColumn('date', 'Date');
+  data.addColumn('number', 'Wait');
+ 
+  // Organize wait data through wait-time servlet
+  fetch(`/visit-date?classCode=` + getParam("classCode"))
+    .then(response => response.json()).then(waits=> {
+    
+    var dates = waits.dates;
+
+    // Convert JSON date format to Date type
+    for (var k = 0; k < dates.length; k++) {
+      var dateStr = dates[k];
+      var realDate = new Date(dateStr);
+      dates[k] = realDate;
+    }
+
+    var waitAverages = waits.waitTimes;
+
+    var tempDataHolder = []; // To be pushed into datatable after updating
+
+    // Loop through both lists and add info sets for each class 
+    for (var i = 0; i < dates.length; i++) {
+      tempDataHolder.push([dates[i], waitAverages[i]]);
+    }
+    
+    data.addRows(tempDataHolder); // Populate datatable with final data
+
+    var options = {
+      title: 'Average Wait Time by Date',
+      hAxis: {
+        format: 'M/d/yy',
+        title: 'Date',
+        textStyle: {
+          bold:true
+        },
+      },
+      vAxis: {
+        title: 'Wait Time (s)',
+        textStyle: {
+          bold:true
+        },
+      },
+      backgroundColor: {
+        gradient: {
+          // Start color for gradient
+          color1: '#C2E1FF',
+          // Finish color for gradient
+          color2: '#2457AA',
+          // Start and end point of gradient, start 
+          // on upper left corner
+          x1: '0%', y1: '0%',
+          x2: '100%', y2: '100%',
+          // If true, the boundary for x1,
+          // y1, x2, and y2 is the box. If
+          // false, it's the entire chart.
+          useObjectBoundingBoxUnits: true
+        },
+        stroke: '#031430',
+        strokeWidth: 5
+      },
+    };
+
+    var chart = new google.visualization.LineChart(
+    document.getElementById("wait-chart"));
+
+    chart.draw(data, options);
+  });
+}
+
 google.charts.setOnLoadCallback(drawBasic);
+google.charts.setOnLoadCallback(drawTime);
 
 // Provide a link to the TA queue and display class code
 function setRedirect(){
