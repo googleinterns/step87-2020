5dae312e2e64dc1d6737d27dabfd819b57405ae4
Nikhil Mittu <nick@mittudev.com>
2020-07-20 22:20:27 +0000

    User Authenticator in EnvironmentServlet.

--- src/main/java/com/google/sps/servlets/EnvironmentServlet.java
+++ src/main/java/com/google/sps/servlets/EnvironmentServlet.java
@@ -4,9 +4,11 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.gson.Gson;
+import com.google.sps.authentication.Authenticator;
 import com.google.sps.environment.Environment;
 import com.google.sps.tasks.TaskSchedulerFactory;
 import java.io.IOException;
@@ -19,31 +21,42 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/environment")
 public class EnvironmentServlet extends HttpServlet {
   private TaskSchedulerFactory taskSchedulerFactory;
+  private Authenticator auth;
   @VisibleForTesting protected String QUEUE_NAME;
 
   @Override
   public void init() throws ServletException {
     taskSchedulerFactory = TaskSchedulerFactory.getInstance();
     QUEUE_NAME = System.getenv("EXECUTION_QUEUE_ID");
+    try {
+      auth = new Authenticator();
+    } catch (IOException e) {
+      throw new ServletException(e);
+    }
   }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     String envID = req.getParameter("envID");
+    String idToken = req.getParameter("idToken");
 
     try {
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       Entity e = datastore.get(KeyFactory.stringToKey(envID));
 
-      resp.getWriter()
-          .print(
-              new Gson()
-                  .toJson(
-                      new Environment(
-                          (String) e.getProperty("name"),
-                          (String) e.getProperty("status"),
-                          KeyFactory.keyToString(e.getKey()))));
+      if (auth.verifyTaOrOwner(idToken, (Key) e.getProperty("class"))) {
+        resp.getWriter()
+            .print(
+                new Gson()
+                    .toJson(
+                        new Environment(
+                            (String) e.getProperty("name"),
+                            (String) e.getProperty("status"),
+                            KeyFactory.keyToString(e.getKey()))));
+      } else {
+        resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+      }
     } catch (EntityNotFoundException | IllegalArgumentException e) {
       resp.sendError(HttpServletResponse.SC_NOT_FOUND);
     }
@@ -54,15 +67,21 @@ public class EnvironmentServlet extends HttpServlet {
       throws ServletException, IOException {
     String envID = req.getParameter("envID");
 
+    String idToken = req.getParameter("idToken");
+
     try {
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       Entity e = datastore.get(KeyFactory.stringToKey(envID));
-      e.setProperty("status", "deleting");
-      datastore.put(e);
+      if (auth.verifyTaOrOwner(idToken, (Key) e.getProperty("class"))) {
+        e.setProperty("status", "deleting");
+        datastore.put(e);
 
-      taskSchedulerFactory.create(QUEUE_NAME, "/tasks/deleteEnv").schedule(envID);
+        taskSchedulerFactory.create(QUEUE_NAME, "/tasks/deleteEnv").schedule(envID);
 
-      resp.getWriter().print(envID);
+        resp.getWriter().print(envID);
+      } else {
+        resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+      }
     } catch (EntityNotFoundException e) {
       resp.sendError(HttpServletResponse.SC_NOT_FOUND);
     }
