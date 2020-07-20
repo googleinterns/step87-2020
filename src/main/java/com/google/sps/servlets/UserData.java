81ed5ac8f148852cd13b36f3ee2dc1e924d4d5c6
Bianca Saputra <biancasap@google.com>
2020-07-20 18:20:04 +0000

    Create helper class to create UserData objects

---
+++ src/main/java/com/google/sps/servlets/UserData.java
@@ -0,0 +1,11 @@
+package com.google.sps.servlets;
+
+public class UserData {
+  private String name;
+  private String code;
+
+  public UserData(String code, String name) {
+    this.name = name;
+    this.code = code;
+  }
+}
