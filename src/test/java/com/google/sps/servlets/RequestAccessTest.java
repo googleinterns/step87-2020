2390d4fc71945a40dd7e4b1db4176614b31cec7a
Nikhil Mittu <nick@mittudev.com>
2020-07-20 21:43:58 +0000

    Add test for tas

--- src/test/java/com/google/sps/servlets/RequestAccessTest.java
+++ src/test/java/com/google/sps/servlets/RequestAccessTest.java
@@ -191,4 +191,77 @@ public class RequestAccessTest {
 
     verify(transportDelegate, times(0)).send(any());
   }
+
+  @Test
+  public void doGetWithTAs() throws Exception {
+    Entity classEntity = new Entity("Class");
+    classEntity.setProperty("owner", OWNER_ID);
+    datastore.put(classEntity);
+
+    Entity classEntity2 = new Entity("Class");
+    classEntity.setProperty("owner", OWNER_ID);
+    datastore.put(classEntity2);
+
+    Entity user = new Entity("User");
+    user.setProperty("userEmail", USER_EMAIL);
+    user.setProperty("registeredClasses", Collections.emptyList());
+    datastore.put(user);
+
+    String TA_EMAIL_1 = "ta1@example.com";
+    String TA_EMAIL_2 = "ta2@example.com";
+
+    Entity ta1 = new Entity("User");
+    ta1.setProperty("userEmail", TA_EMAIL_1);
+    ta1.setProperty("taClasses", Arrays.asList(classEntity.getKey()));
+    datastore.put(ta1);
+
+    Entity ta2 = new Entity("User");
+    ta2.setProperty("userEmail", TA_EMAIL_2);
+    ta2.setProperty("taClasses", Arrays.asList(classEntity2.getKey(), classEntity.getKey()));
+    datastore.put(ta2);
+
+    when(req.getParameter("idToken")).thenReturn(ID_TOKEN);
+    when(req.getParameter("classCode")).thenReturn(KeyFactory.keyToString(classEntity.getKey()));
+    when(req.getScheme()).thenReturn(SCHEME);
+    when(req.getServerName()).thenReturn(SERVER_NAME);
+
+    FirebaseToken tok = mock(FirebaseToken.class);
+    when(auth.verifyIdToken(ID_TOKEN)).thenReturn(tok);
+    when(tok.getEmail()).thenReturn(USER_EMAIL);
+    when(tok.getName()).thenReturn(USER_NAME);
+
+    UserRecord owner = mock(UserRecord.class);
+    when(auth.getUser(OWNER_ID)).thenReturn(owner);
+    when(owner.getEmail()).thenReturn(OWNER_EMAIL);
+    when(owner.getDisplayName()).thenReturn(OWNER_NAME);
+
+    servlet.doGet(req, resp);
+
+    verify(transportDelegate, times(1)).send(msgCaptor.capture());
+
+    Message msg = msgCaptor.getValue();
+
+    assertEquals(new InternetAddress(FROM_EMAIL), msg.getFrom()[0]);
+    assertEquals(
+        new InternetAddress(OWNER_EMAIL, OWNER_NAME),
+        msg.getRecipients(Message.RecipientType.TO)[0]);
+
+    assertTrue(
+        Arrays.asList(msg.getRecipients(Message.RecipientType.CC))
+            .contains(new InternetAddress(TA_EMAIL_1)));
+    assertTrue(
+        Arrays.asList(msg.getRecipients(Message.RecipientType.CC))
+            .contains(new InternetAddress(TA_EMAIL_2)));
+
+    assertTrue(((String) msg.getContent()).contains(USER_EMAIL));
+    assertTrue(
+        ((String) msg.getContent())
+            .contains(
+                new URL(
+                        req.getScheme(),
+                        req.getServerName(),
+                        ApplicationDefaults.DASHBOARD.concat(
+                            KeyFactory.keyToString(classEntity.getKey())))
+                    .toString()));
+  }
 }
