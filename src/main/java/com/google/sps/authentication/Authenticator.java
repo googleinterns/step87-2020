37ade26141f13a3e99d0004f8bd7ad3722a53fa0
Nikhil Mittu <nick@mittudev.com>
2020-07-21 14:19:34 +0000

    Add authenticator method for workspaces.

--- src/main/java/com/google/sps/authentication/Authenticator.java
+++ src/main/java/com/google/sps/authentication/Authenticator.java
@@ -1,9 +1,5 @@
 package com.google.sps.authentication;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.FetchOptions;
@@ -12,11 +8,17 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.google.sps.firebase.FirebaseAppManager;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.sps.workspace.Workspace;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class Authenticator {
   private FirebaseAuth auth;
@@ -31,22 +33,34 @@ public class Authenticator {
     datastore = DatastoreServiceFactory.getDatastoreService();
   }
 
+  public boolean verifyTaOrOwner(String idToken, String classCode) {
+    return verifyTaOrOwner(idToken, KeyFactory.stringToKey(classCode));
+  }
+
   public boolean verifyTaOrOwner(String idToken, Key classKey) {
     try {
       FirebaseToken tok = auth.verifyIdToken(idToken);
-      PreparedQuery q = datastore.prepare(new Query("User").setFilter(
-        new FilterPredicate("userEmail", FilterOperator.EQUAL, tok.getEmail())
-      ));
+      PreparedQuery q = datastore
+          .prepare(new Query("User").setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, tok.getEmail())));
 
-      if(q.countEntities(FetchOptions.Builder.withLimit(1)) > 0) {
-        return ((List<Key>) q.asSingleEntity().getProperty("taClasses")).contains(classKey) ||
-               ((List<Key>) q.asSingleEntity().getProperty("ownedClasses")).contains(classKey);
+      if (q.countEntities(FetchOptions.Builder.withLimit(1)) > 0) {
+        return ((List<Key>) q.asSingleEntity().getProperty("taClasses")).contains(classKey)
+            || ((List<Key>) q.asSingleEntity().getProperty("ownedClasses")).contains(classKey);
       } else {
         return false;
       }
     } catch (FirebaseAuthException | IllegalArgumentException e) {
       return false;
     }
-    
-  } 
+  }
+
+  public boolean verifyWorkspace(String idToken, Workspace w) throws InterruptedException, ExecutionException {
+    try {
+      FirebaseToken tok = auth.verifyIdToken(idToken);
+
+      return w.getStudentUID().get().equals(tok.getUid()) || w.getTaUID().get().equals(tok.getUid());
+    } catch (FirebaseAuthException | IllegalArgumentException e) {
+      return false;
+    }
+  }
 }
