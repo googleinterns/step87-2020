d9d1624fb3a8082ce97c5b8cfb71280b44360ef3
Nikhil Mittu <nick@mittudev.com>
2020-07-22 19:29:03 +0000

    Update Authenticator test to set datastore configurations.

--- src/test/java/com/google/sps/authentication/AuthenticatorTest.java
+++ src/test/java/com/google/sps/authentication/AuthenticatorTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceConfig;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
@@ -47,6 +48,8 @@ public class AuthenticatorTest {
   public void setUp() {
     helper.setUp();
     datastore = DatastoreServiceFactory.getDatastoreService();
+    System.setProperty(
+        DatastoreServiceConfig.DATASTORE_EMPTY_LIST_SUPPORT, Boolean.TRUE.toString());
   }
 
   @After
