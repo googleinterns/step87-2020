8e5995a5da530523ef5bacd9d0b479b0938f552e
Maryam <amaryam294@gmail.com>
2020-07-23 21:46:07 +0000

    Modify code to fix merge conflicts.

--- src/main/webapp/script/auth.js
+++ src/main/webapp/script/auth.js
@@ -2,12 +2,18 @@ const config = {
   apiKey: 'AIzaSyA1r_PfVDCXfTgoUNisci5Ag2MKEEwsZCE',
   databaseURL: "https://fulfillment-deco-step-2020.firebaseio.com",
   projectId: "fulfillment-deco-step-2020",
+  
+
+authDomain: "fulfillment-deco-step-2020.firebaseapp.com",
+storageBucket: "fulfillment-deco-step-2020.appspot.com",
+messagingSenderId: "7165833112",
+appId: "1:7165833112:web:3b4af53c5de6aa73b7c5ed"
 };
 firebase.initializeApp(config);
 
 // If user is not logged in, redirect to the home page
 firebase.auth().onAuthStateChanged(function(user) {
-  if (!user) {
+  if (!user && (window.location.pathname !== "/")) {
     window.location.href = "/";
   }
   else {
