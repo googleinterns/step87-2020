67a6354c7f859495cac293422f64269610382298
Nikhil Mittu <nick@mittudev.com>
2020-07-27 16:59:49 +0000

    Update UserData servlet to return if the studnet is on the queue or not.

--- src/main/java/com/google/sps/models/UserData.java
+++ src/main/java/com/google/sps/models/UserData.java
@@ -4,10 +4,16 @@ public class UserData {
   private String name;
   private String code;
   private String type;
+  private boolean inQueue;
 
   public UserData(String code, String name, String type) {
+    this(code, name, type, false);
+  }
+
+  public UserData(String code, String name, String type, boolean inQueue) {
     this.name = name;
     this.code = code;
     this.type = type;
+    this.inQueue = inQueue;
   }
 }
