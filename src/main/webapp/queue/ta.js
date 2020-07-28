e0da4062905d0cdc7186efe72d272f8b04d56fa0
Bianca Saputra <biancasap@google.com>
2020-07-28 20:46:23 +0000

    resolve jshint build

--- src/main/webapp/queue/ta.js
+++ src/main/webapp/queue/ta.js
@@ -82,12 +82,11 @@ function getQueue() {
         queueListElement.appendChild(createListElement(studentEmail));
     });
 
+    const beinghelpedElem = document.getElementById('beingHelped');
     if (queue.helping) {
-        const beinghelpedElem = document.getElementById('beingHelped');
         beinghelpedElem.innerHTML = "";
         document.getElementById('beingHelped').appendChild(createHelpedElem(queue.helping.email, queue.helping.workspace));
-    }else {
-        const beinghelpedElem = document.getElementById('beingHelped');
+    } else {
         beinghelpedElem.innerHTML = "";
     }
     });
