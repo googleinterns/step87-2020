06888b5f65a527ae442d5462f545cf5cf12d761d
Nikhil Mittu <nick@mittudev.com>
2020-07-30 17:42:16 +0000

    Update unit tests for Environment error messages.

--- src/test/java/com/google/sps/servlets/course/EnvironmentServletTest.java
+++ src/test/java/com/google/sps/servlets/course/EnvironmentServletTest.java
@@ -166,4 +166,30 @@ public class EnvironmentServletTest {
 
     verify(resp, times(1)).sendError(HttpServletResponse.SC_FORBIDDEN);
   }
+
+  @Test
+  public void doGetTestWithError() throws Exception {
+    String STATUS = "STATUS";
+    String NAME = "NAME";
+    String ERROR = "ERROR";
+
+    Key classKey = datastore.put(new Entity("class"));
+
+    Entity envEntity = new Entity("Environment");
+    envEntity.setProperty("status", STATUS);
+    envEntity.setProperty("name", NAME);
+    envEntity.setProperty("class", classKey);
+    envEntity.setProperty("error", ERROR);
+    String envID = KeyFactory.keyToString(datastore.put(envEntity));
+
+    when(req.getParameter(eq("envID"))).thenReturn(envID);
+    when(req.getParameter(eq("idToken"))).thenReturn(ID_TOKEN);
+    when(auth.verifyInClass(eq(ID_TOKEN), eq(classKey))).thenReturn(true);
+    when(resp.getWriter()).thenReturn(printWriter);
+
+    servlet.doGet(req, resp);
+
+    verify(printWriter, times(1))
+        .print(new Gson().toJson(new Environment(NAME, STATUS, ERROR, envID)));
+  }
 }
--- src/test/java/com/google/sps/servlets/course/GetEnvironmentsTest.java
+++ src/test/java/com/google/sps/servlets/course/GetEnvironmentsTest.java
@@ -198,4 +198,64 @@ public class GetEnvironmentsTest {
 
     verify(resp, times(1)).sendError(HttpServletResponse.SC_FORBIDDEN);
   }
+
+  @Test
+  public void doGetWithError() throws Exception {
+    Entity classEntity = new Entity("Class");
+    Entity classEntity2 = new Entity("Class");
+    datastore.put(classEntity);
+    datastore.put(classEntity2);
+
+    String STATUS1 = "STATUS1";
+    String STATUS2 = "STATUS2";
+    String STATUS3 = "STATUS3";
+    String NAME1 = "NAME1";
+    String NAME2 = "NAME2";
+    String NAME3 = "NAME3";
+    String IMAGE1 = "IMAGE1";
+    String IMAGE2 = "IMAGE2";
+    String IMAGE3 = "IMAGE3";
+    String ERROR = "ERROR";
+
+    Entity env1 = new Entity("Environment");
+    env1.setProperty("name", NAME1);
+    env1.setProperty("status", STATUS1);
+    env1.setProperty("image", IMAGE1);
+    env1.setProperty("class", classEntity.getKey());
+
+    Entity env2 = new Entity("Environment");
+    env2.setProperty("name", NAME2);
+    env2.setProperty("status", STATUS2);
+    env2.setProperty("image", IMAGE2);
+    env2.setProperty("error", ERROR);
+    env2.setProperty("class", classEntity.getKey());
+
+    Entity env3 = new Entity("Environment");
+    env3.setProperty("name", NAME3);
+    env3.setProperty("status", STATUS3);
+    env3.setProperty("image", IMAGE3);
+    env3.setProperty("class", classEntity2.getKey());
+
+    datastore.put(env1);
+    datastore.put(env2);
+    datastore.put(env3);
+
+    when(req.getParameter(eq("classID"))).thenReturn(KeyFactory.keyToString(classEntity.getKey()));
+    when(req.getParameter(eq("idToken"))).thenReturn(ID_TOKEN);
+    when(auth.verifyInClass(eq(ID_TOKEN), eq(KeyFactory.keyToString(classEntity.getKey()))))
+        .thenReturn(true);
+    when(resp.getWriter()).thenReturn(writer);
+
+    servlet.doGet(req, resp);
+
+    verify(writer, times(1))
+        .print(
+            new Gson()
+                .toJson(
+                    Arrays.asList(
+                        new Environment(
+                            NAME1, STATUS1, null, KeyFactory.keyToString(env1.getKey())),
+                        new Environment(
+                            NAME2, STATUS2, ERROR, KeyFactory.keyToString(env2.getKey())))));
+  }
 }
