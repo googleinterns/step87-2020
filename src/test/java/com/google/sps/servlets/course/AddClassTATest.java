423e796340adee606a82830dffb4361684b9410a
Maryam <amaryam294@gmail.com>
2020-08-03 17:22:48 +0000

    Refactor TA tests.

--- src/test/java/com/google/sps/servlets/course/AddClassTATest.java
+++ src/test/java/com/google/sps/servlets/course/AddClassTATest.java
@@ -51,11 +51,64 @@ public class AddClassTATest {
   @InjectMocks AddClassTA addTA;
 
   private final String ID_TOKEN = "ID_TOKEN";
+  private final String TA_EMAIL = "test@google.com";
+  private final String EMAIL_2 = "test2@google.com";
+
+  private Entity init;
+  private Entity init2;
+  private Entity init3;
+  private Entity init4;
+  private Entity user;
+  private Entity user2;
 
   @Before
   public void setUp() {
     helper.setUp();
     datastore = DatastoreServiceFactory.getDatastoreService();
+
+    // Create classes
+    init = new Entity("Class");
+
+    init.setProperty("owner", "ownerID");
+    init.setProperty("name", "testClass");
+    init.setProperty("beingHelped", new EmbeddedEntity());
+    init.setProperty("studentQueue", Collections.emptyList());
+
+    init2 = new Entity("Class");
+
+    init2.setProperty("owner", "ownerID2");
+    init2.setProperty("name", "testClass2");
+    init2.setProperty("beingHelped", new EmbeddedEntity());
+    init2.setProperty("studentQueue", Collections.emptyList());
+
+    init3 = new Entity("Class");
+
+    init3.setProperty("owner", "ownerID3");
+    init3.setProperty("name", "testClass3");
+    init3.setProperty("beingHelped", new EmbeddedEntity());
+    init3.setProperty("studentQueue", Collections.emptyList());
+
+    init4 = new Entity("Class");
+
+    init4.setProperty("owner", "ownerID4");
+    init4.setProperty("name", "testClass4");
+    init4.setProperty("beingHelped", new EmbeddedEntity());
+    init4.setProperty("studentQueue", Collections.emptyList());
+
+    // Create users
+    user = new Entity("User");
+
+    user.setProperty("userEmail", TA_EMAIL);
+    user.setProperty("registeredClasses", Collections.emptyList());
+    user.setProperty("taClasses", Collections.emptyList());
+    user.setProperty("ownedClasses", Collections.emptyList());
+
+    user2 = new Entity("User");
+
+    user2.setProperty("userEmail", EMAIL_2);
+    user2.setProperty("registeredClasses", Collections.emptyList());
+    user2.setProperty("taClasses", Collections.emptyList());
+    user2.setProperty("ownedClasses", Collections.emptyList());
   }
 
   @After
@@ -67,28 +120,11 @@ public class AddClassTATest {
   // For a user that doesn't TA for any class, add a class
   public void addOneTAEmptyList() throws Exception {
 
-    // Create a class
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
-
     datastore.put(init);
-
-    // Create a user
-    Entity user = new Entity("User");
-
-    user.setProperty("userEmail", "test@google.com");
-    user.setProperty("registeredClasses", Collections.emptyList());
-    user.setProperty("taClasses", Collections.emptyList());
-    user.setProperty("ownedClasses", Collections.emptyList());
-
     datastore.put(user);
 
     // Create examples for the TA email and class code
-    when(httpRequest.getParameter("taEmail")).thenReturn("test@google.com");
+    when(httpRequest.getParameter("taEmail")).thenReturn(TA_EMAIL);
     when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
     when(httpRequest.getParameter("idToken")).thenReturn(ID_TOKEN);
     when(auth.verifyTaOrOwner(ID_TOKEN, KeyFactory.keyToString(init.getKey()))).thenReturn(true);
@@ -99,8 +135,7 @@ public class AddClassTATest {
     PreparedQuery queryUser =
         datastore.prepare(
             new Query("User")
-                .setFilter(
-                    new FilterPredicate("userEmail", FilterOperator.EQUAL, "test@google.com")));
+                .setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, TA_EMAIL)));
 
     Entity userTA = queryUser.asSingleEntity();
 
@@ -113,39 +148,23 @@ public class AddClassTATest {
   // For a user that already TAs for one class, add another class
   public void addOneTANonEmptyList() throws Exception {
 
-    // Create a ta and non-ta class
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
-
-    Entity init2 = new Entity("Class");
-
-    init2.setProperty("owner", "ownerID2");
-    init2.setProperty("name", "testClass2");
-    init2.setProperty("beingHelped", new EmbeddedEntity());
-    init2.setProperty("studentQueue", Collections.emptyList());
-    init2.setProperty("taList", Collections.emptyList());
-
     datastore.put(init);
     datastore.put(init2);
 
     // Initialize a user
-    Entity user = new Entity("User");
+    Entity testUser = new Entity("User");
 
     List<Key> taClassList = Arrays.asList(init.getKey());
 
-    user.setProperty("userEmail", "test@google.com");
-    user.setProperty("registeredClasses", Collections.emptyList());
-    user.setProperty("taClasses", taClassList);
-    user.setProperty("ownedClasses", Collections.emptyList());
+    testUser.setProperty("userEmail", TA_EMAIL);
+    testUser.setProperty("registeredClasses", Collections.emptyList());
+    testUser.setProperty("taClasses", taClassList);
+    testUser.setProperty("ownedClasses", Collections.emptyList());
 
-    datastore.put(user);
+    datastore.put(testUser);
 
     // Create examples for the TA email and class code
-    when(httpRequest.getParameter("taEmail")).thenReturn("test@google.com");
+    when(httpRequest.getParameter("taEmail")).thenReturn(TA_EMAIL);
     when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init2.getKey()));
     when(httpRequest.getParameter("idToken")).thenReturn(ID_TOKEN);
     when(auth.verifyTaOrOwner(eq(ID_TOKEN), eq(KeyFactory.keyToString(init2.getKey()))))
@@ -157,8 +176,7 @@ public class AddClassTATest {
     PreparedQuery queryUser =
         datastore.prepare(
             new Query("User")
-                .setFilter(
-                    new FilterPredicate("userEmail", FilterOperator.EQUAL, "test@google.com")));
+                .setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, TA_EMAIL)));
 
     Entity userTA = queryUser.asSingleEntity();
 
@@ -172,38 +190,23 @@ public class AddClassTATest {
   // Verify that duplicate classes don't get added
   public void preventDuplicates() throws Exception {
 
-    // Create some classes
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
-
-    Entity init2 = new Entity("Class");
-
-    init2.setProperty("owner", "ownerID2");
-    init2.setProperty("name", "testClass2");
-    init2.setProperty("beingHelped", new EmbeddedEntity());
-    init2.setProperty("studentQueue", Collections.emptyList());
-
     datastore.put(init);
     datastore.put(init2);
 
     // Initialize a TA user
-    Entity user = new Entity("User");
+    Entity testUser = new Entity("User");
 
     List<Key> taClassList = Arrays.asList(init.getKey(), init2.getKey());
 
-    user.setProperty("userEmail", "testTA@google.com");
-    user.setProperty("registeredClasses", Collections.emptyList());
-    user.setProperty("taClasses", taClassList);
-    user.setProperty("ownedClasses", Collections.emptyList());
+    testUser.setProperty("userEmail", TA_EMAIL);
+    testUser.setProperty("registeredClasses", Collections.emptyList());
+    testUser.setProperty("taClasses", taClassList);
+    testUser.setProperty("ownedClasses", Collections.emptyList());
 
-    datastore.put(user);
+    datastore.put(testUser);
 
     // Create examples for the TA email and class code
-    when(httpRequest.getParameter("taEmail")).thenReturn("testTA@google.com");
+    when(httpRequest.getParameter("taEmail")).thenReturn(TA_EMAIL);
     when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
     when(httpRequest.getParameter("idToken")).thenReturn(ID_TOKEN);
     when(auth.verifyTaOrOwner(ID_TOKEN, KeyFactory.keyToString(init.getKey()))).thenReturn(true);
@@ -214,8 +217,7 @@ public class AddClassTATest {
     PreparedQuery queryUser =
         datastore.prepare(
             new Query("User")
-                .setFilter(
-                    new FilterPredicate("userEmail", FilterOperator.EQUAL, "testTA@google.com")));
+                .setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, TA_EMAIL)));
 
     Entity userTA = queryUser.asSingleEntity();
 
@@ -230,53 +232,25 @@ public class AddClassTATest {
   // Add multiple classes for a TA user
   public void addMultipleClassKeys() throws Exception {
 
-    // Create multiple classes
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
-
-    Entity init2 = new Entity("Class");
-
-    init2.setProperty("owner", "ownerID2");
-    init2.setProperty("name", "testClass2");
-    init2.setProperty("beingHelped", new EmbeddedEntity());
-    init2.setProperty("studentQueue", Collections.emptyList());
-    Entity init3 = new Entity("Class");
-
-    init3.setProperty("owner", "ownerID3");
-    init3.setProperty("name", "testClass3");
-    init3.setProperty("beingHelped", new EmbeddedEntity());
-    init3.setProperty("studentQueue", Collections.emptyList());
-
-    Entity init4 = new Entity("Class");
-
-    init4.setProperty("owner", "ownerID4");
-    init4.setProperty("name", "testClass4");
-    init4.setProperty("beingHelped", new EmbeddedEntity());
-    init4.setProperty("studentQueue", Collections.emptyList());
-
     datastore.put(init);
     datastore.put(init2);
     datastore.put(init3);
     datastore.put(init4);
 
     // Create a user
-    Entity user = new Entity("User");
+    Entity testUser = new Entity("User");
 
     List<Key> taClassList = Arrays.asList(init.getKey(), init2.getKey(), init3.getKey());
 
-    user.setProperty("userEmail", "test@google.com");
-    user.setProperty("registeredClasses", Collections.emptyList());
-    user.setProperty("taClasses", taClassList);
-    user.setProperty("ownedClasses", Collections.emptyList());
+    testUser.setProperty("userEmail", TA_EMAIL);
+    testUser.setProperty("registeredClasses", Collections.emptyList());
+    testUser.setProperty("taClasses", taClassList);
+    testUser.setProperty("ownedClasses", Collections.emptyList());
 
-    datastore.put(user);
+    datastore.put(testUser);
 
     // Create examples for the TA email and class code
-    when(httpRequest.getParameter("taEmail")).thenReturn("test@google.com");
+    when(httpRequest.getParameter("taEmail")).thenReturn(TA_EMAIL);
     when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init4.getKey()));
     when(httpRequest.getParameter("idToken")).thenReturn(ID_TOKEN);
     when(auth.verifyTaOrOwner(ID_TOKEN, KeyFactory.keyToString(init4.getKey()))).thenReturn(true);
@@ -287,8 +261,7 @@ public class AddClassTATest {
     PreparedQuery queryUser =
         datastore.prepare(
             new Query("User")
-                .setFilter(
-                    new FilterPredicate("userEmail", FilterOperator.EQUAL, "test@google.com")));
+                .setFilter(new FilterPredicate("userEmail", FilterOperator.EQUAL, TA_EMAIL)));
 
     Entity userTA = queryUser.asSingleEntity();
 
@@ -305,19 +278,12 @@ public class AddClassTATest {
   @Test
   // Throw an exception if class key isn't correct
   public void keyUnavailable() throws Exception {
-    // Create multiple classes
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
 
     datastore.put(init);
     datastore.delete(init.getKey());
 
     // Create examples for the TA email and class code
-    when(httpRequest.getParameter("taEmail")).thenReturn("test@google.com");
+    when(httpRequest.getParameter("taEmail")).thenReturn(TA_EMAIL);
     when(httpRequest.getParameter("classCode")).thenReturn(KeyFactory.keyToString(init.getKey()));
     when(httpRequest.getParameter("idToken")).thenReturn(ID_TOKEN);
     when(auth.verifyTaOrOwner(ID_TOKEN, KeyFactory.keyToString(init.getKey()))).thenReturn(true);
@@ -327,38 +293,12 @@ public class AddClassTATest {
     verify(httpResponse).sendError(HttpServletResponse.SC_NOT_FOUND);
   }
 
-  // Add multiple TA's at the same time
   @Test
+  // Add multiple TA's at the same time
   public void addOneMultiple() throws Exception {
 
-    // Create a class
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
-
     datastore.put(init);
-
-    // Create a user
-    Entity user = new Entity("User");
-
-    user.setProperty("userEmail", "test@google.com");
-    user.setProperty("registeredClasses", Collections.emptyList());
-    user.setProperty("taClasses", Collections.emptyList());
-    user.setProperty("ownedClasses", Collections.emptyList());
-
     datastore.put(user);
-
-    // Create a user
-    Entity user2 = new Entity("User");
-
-    user2.setProperty("userEmail", "test2@google.com");
-    user2.setProperty("registeredClasses", Collections.emptyList());
-    user2.setProperty("taClasses", Collections.emptyList());
-    user2.setProperty("ownedClasses", Collections.emptyList());
-
     datastore.put(user2);
 
     // Create examples for the TA email and class code
@@ -381,38 +321,12 @@ public class AddClassTATest {
     assertTrue(taClasses2.size() == 1);
   }
 
-  // Add multiple TA's at the same time with whitespace
   @Test
+  // Add multiple TA's at the same time with whitespace
   public void addOneMultipleWhitespace() throws Exception {
 
-    // Create a class
-    Entity init = new Entity("Class");
-
-    init.setProperty("owner", "ownerID");
-    init.setProperty("name", "testClass");
-    init.setProperty("beingHelped", new EmbeddedEntity());
-    init.setProperty("studentQueue", Collections.emptyList());
-
     datastore.put(init);
-
-    // Create a user
-    Entity user = new Entity("User");
-
-    user.setProperty("userEmail", "test@google.com");
-    user.setProperty("registeredClasses", Collections.emptyList());
-    user.setProperty("taClasses", Collections.emptyList());
-    user.setProperty("ownedClasses", Collections.emptyList());
-
     datastore.put(user);
-
-    // Create a user
-    Entity user2 = new Entity("User");
-
-    user2.setProperty("userEmail", "test2@google.com");
-    user2.setProperty("registeredClasses", Collections.emptyList());
-    user2.setProperty("taClasses", Collections.emptyList());
-    user2.setProperty("ownedClasses", Collections.emptyList());
-
     datastore.put(user2);
 
     when(httpRequest.getParameter("taEmail")).thenReturn("test@google.com, \n\t test2@google.com");
